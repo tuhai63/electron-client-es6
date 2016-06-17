ca6bf12a8a1cb4ed4ed43f88407d46f2fca6cca2
wei fang <wfang.ca@gmail.com>
2016-06-17 17:49:16 -0400

    dev updates

--- app/browser/scripts/legal_entity/legalEntityService.js
+++ app/browser/scripts/legal_entity/legalEntityService.js
@@ -1,17 +1,15 @@
-import uuid from 'node-uuid';
-import {LegalEntityIdentifier, ContactPerson, ContactAddress, LegalEntity} from './legalEntityModel';
-
 class LegalEntityService {
     constructor($http) {        
         this.$http = $http;
     }
-
-    // return a list of all legal entities
-    getLegalEntities() {   
+    
+    getLegalEntities() {
+        // return a list of all legal entities   
         return this.$http.get('http://localhost:3000/api/legal_entities');
     }
 
     getLegalEntityByName(name) {
+        // return a list of legal entities by name query
         return this.$http.get('http://localhost:3000/api/legal_entities/name/' + name);
     }
     
@@ -26,50 +24,19 @@ class LegalEntityService {
     }
 
     updateLegalEntity(legalEntity) {
+        // update le by replacing entire legalEntity
         return this.$http.put('http://localhost:3000/api/legal_entities', legalEntity );
-
-        /*
-        // $http returns a promise, which has a then function, which also returns a promise
-        let promise = this.$http.put('http://localhost:3000/api/legal_entities', legalEntity ).then(function (response) {
-            // The then function here is an opportunity to modify the response
-            console.log(response);
-            // The return value gets picked up by the then in the controller.
-            return response.data;
-        });
-        // Return the promise to the controller
-        return promise;
-        */
     }
 
     createLegalEntity(legalEntity) { 
+        // create a new legal entity
         return this.$http.post('http://localhost:3000/api/legal_entities', legalEntity );
     }
 
-    deleteLegalEntity(id) {            
+    deleteLegalEntity(id) {  
+        // delete a legal entity by database id          
         return this.$http.delete('http://localhost:3000/api/legal_entities/' + id );
     }
-
-    /*
-    // return a list of legal entities in role of 'Regulatory Authority'
-    getRegulatoryAuthorities() {        
-        let deferred = this.$q.defer();
-        this.legalEntities.find({'LEGALENTITY_TYPE.VALUE': 'Regulatory Authority'}, function (err, rows) {
-            if (err) deferred.reject(err);
-            deferred.resolve(rows);
-        });      
-        return deferred.promise;  
-    }
-    
-    // return a list of legal entities NOT in role of 'Regulatory Authority'
-    getNonRALegalEntities() {        
-        let deferred = this.$q.defer();
-        this.legalEntities.find({'LEGALENTITY_TYPE.VALUE': {$ne : 'Regulatory Authority'}}, function (err, rows) {
-            if (err) deferred.reject(err);
-            deferred.resolve(rows);
-        });      
-        return deferred.promise;  
-    }
-    */
 }
 
 LegalEntityService.$inject = ['$http'];
